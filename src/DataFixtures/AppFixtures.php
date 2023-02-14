2ada3c13dc48884004108e0269adb9d7a6b5eb86
MEHDI BELLAM <porcepic91@gmail.com>
2023-02-14 11:36:51 +0100

    :card_file_box: ajout de des données pour la table Products :card_file_box:

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ProductCategories;
+use App\Entity\Products;
+use App\Entity\Users;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -12,6 +16,56 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
+        for ($i = 0; $i < 50; $i++) {
+            $user = new Users();
+            if ($i == 0) {
+                $user->setRole('ADMIN');
+                $user->setmail("admin@localhost");
+                $user->setPassword('admin');
+                $manager->persist($user);
+
+            }else {
+
+                $user->setRole('USER');
+                $user->setmail("user" .$i . "@localhost");
+                $user->setPassword('user' . $i);
+                $manager->persist($user);
+            }
+
+        }
+        $manager->flush();
+
+        $categories = array("jouet", "vetement", "alimentation", "mobilier", "electronique", "sport", "livres",
+            "jeux video", "DVD et Blu-ray", "autre ");
+
+
+        foreach ($categories as $categorie) {
+            $category = new ProductCategories();
+            $category->setName($categorie);
+
+            $manager->persist($category);
+        }
+
+
+        $manager->flush();
+
+        $AllProductCategories = $manager->getRepository(ProductCategories::class)->findAll();
+
+
+        for ($i = 0; $i < 200; $i++) {
+            $product = new Products();
+            $product->setName("produit" . $i);
+
+            $product->setCategories($AllProductCategories[rand(0, count($AllProductCategories) - 1)]);
+
+
+            $product->setPrice(rand(1, 1000));
+
+            $product->setDescription("description of the N' " . $i . " product ");
+            $product->setStock(rand(1, 1000));
+
+            $manager->persist($product );
+        }
         $manager->flush();
     }
 }
--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -19,6 +19,11 @@ class Users
     #[ORM\Column(length: 255)]
     private ?string $mail = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $role = null;
+
+
+
     public function getId(): ?int
     {
         return $this->id;
@@ -44,6 +49,18 @@ class Users
     public function setMail(string $mail): self
     {
         $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
 
         return $this;
     }
